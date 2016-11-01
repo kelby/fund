409335f7874b3871b05ed8ff5579a607e239d549
Kelby Lee <leekelby@gmail.com>
2016-11-01 22:42:23 +0800

    config deploy share linked

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,10 +24,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/sockets' #, 'tmp/cache', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
