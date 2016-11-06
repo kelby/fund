0be1f314ee418938983245a7c262fcfab33bc26a
Kelby Lee <leekelby@gmail.com>
2016-11-06 21:11:05 +0800

    优化一下项目新建页面

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,39 @@
+// begin new project
+$(document).on("change", "#language", function() {
+  var language = $(this).val()
+
+  if (language.length > 0) {
+    $("#catalog_id").prop('disabled', false)
+  } else {
+    $("#catalog_id").prop('disabled', true)
+    return
+  }
+
+  var catalog_id = "#" + language + "_catalog"
+
+  $("#catalog_id").html( $(catalog_id).html() )
+
+  $("#project_category_id").val('')
+  $("#project_category_id option").addClass('hidden')
+  $("#project_category_id option:first").removeClass('hidden')
+})
+
+$(document).on("change", "#catalog_id", function() {
+  var catalog = $(this).val()
+
+  if (catalog.length > 0) {
+    $("#project_category_id").prop('disabled', false)
+  } else {
+    $("#project_category_id").prop('disabled', true)
+    return
+  }
+
+  $("#project_category_id option").addClass('hidden')
+
+  var category_id = "." + "catalog-" + catalog
+  $(category_id).removeClass('hidden')
+
+  $("#project_category_id").val('')
+  $("#project_category_id option:first").removeClass('hidden')
+})
+// end new project
