3f13025aae92a22833919f9cdde0e5b4acd3dbfe
Kelby Lee <leekelby@gmail.com>
2016-11-06 21:23:16 +0800

    fix project create bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -62,6 +62,7 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :website, :wiki, :source_code, :category_id)
+      params.require(:project).permit(:name, :description, :website, :wiki, :source_code,
+        :category_id)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :category
+
+  validates_presence_of :source_code
+  validates_presence_of :category_id
 end
