7666cb2f8ce9924bb867f1aa662fb853cb44fa81
Kelby Lee <leekelby@gmail.com>
2016-11-25 14:25:40 +0800

    sidekiq has bug, need it.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, 'git@bitbucket.org:kelbylee/zhenkuan.git'
 # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/settings/production.yml'
