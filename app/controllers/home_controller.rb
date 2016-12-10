9e410bf2a50731e61b38b0186021f8003a6897ae
Kelby Lee <leekelby@gmail.com>
2016-12-10 16:26:54 +0800

    把php packages先去掉

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @gems = Project.gem.limit(3)
-    @packages = Project.package.limit(3)
+    # @packages = Project.package.limit(3)
     @pods = Project.pod.limit(3)
   end
 end
