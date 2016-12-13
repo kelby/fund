98427aff6ae1331b5846f2709dc1ea90f8973b66
Kelby Lee <leekelby@gmail.com>
2016-12-13 19:00:32 +0800

    类目里项目按流行排序

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1
   def show
-    @projects = @category.projects.online
+    @projects = @category.projects.online.order(popularity: :desc)
   end
 
   # GET /categories/new
