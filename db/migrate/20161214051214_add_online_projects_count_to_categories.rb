a2ddf302224750a80fb704a131c05b03c70d8661
Kelby Lee <leekelby@gmail.com>
2016-12-14 14:56:06 +0800

    迁移的时候，顺便把计数器也设置了吧

--- db/migrate/20161214051214_add_online_projects_count_to_categories.rb
+++ db/migrate/20161214051214_add_online_projects_count_to_categories.rb
@@ -1,5 +1,9 @@
 class AddOnlineProjectsCountToCategories < ActiveRecord::Migration[5.0]
   def change
     add_column :categories, :online_projects_count, :integer, default: 0, null: false
+
+    Category.reset_column_information
+
+    Project.counter_culture_fix_counts
   end
 end
