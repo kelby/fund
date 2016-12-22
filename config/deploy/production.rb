244a58e959638db9e15e69ade67c6efd8e7dfde6
Kelby Lee <leekelby@gmail.com>
2016-12-22 17:28:20 +0800

    部署服用器使用ip，因为有时dns解析不好

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'dev-toolbox.co', user: 'deployer', roles: %w{app db web} #, my_property: :my_value
+server '139.162.90.85', user: 'deployer', roles: %w{app db web} #, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
