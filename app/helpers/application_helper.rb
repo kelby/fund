8cd749a9dcfb291548f9579ace713df1188d7823
Kelby Lee <leekelby@gmail.com>
2016-12-22 10:16:05 +0800

    样式小改，分页进行移动适配

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def magic_paginate(scope, options = {}, &block)
+    if browser.device.mobile?
+      options[:window] = 1
+    else
+      options[:window] = 3
+    end
+
+    paginate(scope, options, &block)
+  end
 end
