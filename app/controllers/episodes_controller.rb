9fc59dc69d7c4f4e8ce37aecd11e8449dd11ae19
Kelby Lee <leekelby@gmail.com>
2016-12-28 16:56:55 +0800

    修复数据获取错误导致小错误

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -65,7 +65,7 @@ class EpisodesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_episode
-      @episode = Episode.find(params[:id])
+      @episode = Episode.find_by(human_id: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
