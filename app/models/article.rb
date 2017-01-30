7c74339afa7f34cabf35960661293197badcf02b
Kelby Lee <leekelby@gmail.com>
2017-01-30 14:10:37 +0800

    annotate model, validates article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Article < ApplicationRecord
+  validates_presence_of :title, :description
 end
