4624076f635e83f7f3bcce6d2a0c467b05cfdb2b
Kelby Lee <leekelby@gmail.com>
2017-02-24 14:10:05 +0800

    净值变更页面代码重构

--- app/controllers/project_arithmetic_controller.rb
+++ app/controllers/project_arithmetic_controller.rb
@@ -2,6 +2,7 @@ class ProjectArithmeticController < ApplicationController
   before_action :set_project
 
   def calculus
+    @fund_yields = @project.fund_yields.order(yield_type: :asc)
   end
 
   private
