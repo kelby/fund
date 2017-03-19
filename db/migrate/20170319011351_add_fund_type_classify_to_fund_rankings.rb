0ea68e093de8673016cca766d7974f263abfffe5
Kelby Lee <leekelby@gmail.com>
2017-03-19 11:41:34 +0800

    迁移太花时间了，并且不是很必须的，放到后面再跑

--- db/migrate/20170319011351_add_fund_type_classify_to_fund_rankings.rb
+++ db/migrate/20170319011351_add_fund_type_classify_to_fund_rankings.rb
@@ -5,17 +5,17 @@ class AddFundTypeClassifyToFundRankings < ActiveRecord::Migration[5.0]
     add_index :fund_rankings, :fund_type_classify
     add_index :fund_rankings, [:record_at, :fund_type_classify]
 
-    FundRanking.reset_column_information
+    # FundRanking.reset_column_information
 
-    FundRanking.fund_type_unknow.find_each do |fr|
-      type_method = Pinyin.t(fr.format_fund_type, splitter: "_").downcase
+    # FundRanking.fund_type_unknow.find_each do |fr|
+    #   type_method = Pinyin.t(fr.format_fund_type, splitter: "_").downcase
 
-      if FundRanking.fund_type_classifies.keys.include?(type_method)
-        new_method = "#{type_method}!"
-        fr.send(new_method)
-      else
-        # fr.fund_type_unknow!
-      end
-    end
+    #   if FundRanking.fund_type_classifies.keys.include?(type_method)
+    #     new_method = "#{type_method}!"
+    #     fr.send(new_method)
+    #   else
+    #     # fr.fund_type_unknow!
+    #   end
+    # end
   end
 end
