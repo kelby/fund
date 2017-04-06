6078a824299221ff2975fc889ffd0269b9ec44c8
Kelby Lee <leekelby@gmail.com>
2017-04-06 08:38:13 +0800

    新网站主题one_page，集成Unify前端模板后更好看、更专业了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout 'one_page'
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :get_today_recommend_projects, only: [:index]
+  layout 'one_page'
 
   def index
   end
