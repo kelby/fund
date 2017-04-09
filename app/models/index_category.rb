68d5d67051541d0e809571798bcc4afe94ac6354
Kelby Lee <leekelby@gmail.com>
2017-04-09 23:47:44 +0800

    小类目名字限制条件应该有大类目

--- app/models/index_category.rb
+++ app/models/index_category.rb
@@ -15,7 +15,7 @@
 
 class IndexCategory < ApplicationRecord
   validates_presence_of :name
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, scope: :index_catalog_id
 
   has_many :index_reports
   belongs_to :index_catalog
