e2bdb60d7796e2bd2170b1cef866863fb7f09efd
Kelby Lee <leekelby@gmail.com>
2017-04-23 13:17:10 +0800

    顶部toolbar样式小改，登录后状态修正

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,6 +31,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+  version :xs do
+    process resize_to_fit: [25, 25]
+  end
+
   version :thumb do
     process resize_to_fit: [50, 50]
   end
